70195f0e35aca5332f1723b77d8933b373f30357
YANO Tetsurou <tetuyano@gmail.com>
2017-03-06 16:17:00 +0900

    Add EventNo_Name.yaml Change EventNo_Name list to load Yaml file.

--- connpass_event_userStatistics.py
+++ connpass_event_userStatistics.py
@@ -3,42 +3,26 @@
 
 import csv
 import os,sys
+import yaml
 import pandas as pd
 from functools import reduce
 
-#C:\work\work\connpass-userstat\fulldata\utf8
-path = 'C:/work/connpass-userstat/fulldata/utf8/meetup'
+path = 'C:/work/connpass-userstat/fulldata/'
 
-EventNo_Name = { 
-  "41997": "もくもく勉強会 #1",
-  "44092": "Meetup Tokyo #2",
-  "46208": "Meetup Tokyo #2懇親会",
-  "45768": "Meetup Tokyo #3",
-  "47871": "もくもく勉強会 #2",
-  "47273": "Meetup Tokyo #4",
-  "50977": "Meetup Tokyo #4懇親会",
-  "49891": "もくもく勉強会 #3",
-  "50984": "もくもく勉強会 #3懇親会",
-  "49614": "Meetup Tokyo #5",
-  "51641": "Meetup Tokyo #5懇親会"
-}
+EventNo_Name = yaml.load(open(path + "EventNo_Name.yaml"))
+#UserDetail = yaml.load(open(path + "UserDetail.yaml"))
 
-FileList = os.listdir(path)
 Events = []
 results = pd.DataFrame()
 
-
-for FileName in FileList:
-    ReadFile = open(FileName, 'r')
-    EventNo = FileName.split('_')[1]
+for EventNo in EventNo_Name.keys():
+    ReadFile = open(path + 'event_' + str(EventNo) + '_participants.csv', 'r')
     print(EventNo)
     dataset1 = pd.read_csv(ReadFile)
     dataset = dataset1.drop("参加枠名",axis=1).drop("受付番号",axis=1).drop("コメント",axis=1).drop("更新日時",axis=1).drop("出欠ステータス",axis=1)
     dataset.rename(columns = {'参加ステータス':'%s' % EventNo_Name[EventNo]},inplace = True)
-#    dataset.rename(columns = {'出欠ステータス':'%s' % EventNo_Name[EventNo]},inplace = True)
     Events.append(dataset)
     ReadFile.close()
-
 
 results = reduce(lambda left,right: pd.merge(left,right,on = ['ユーザー名','表示名'],how='outer'), Events)
 
